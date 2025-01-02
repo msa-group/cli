3c12b06faf6199f0af423a604cf0f46242b89ffc
kaplax <eumenides_x@icloud.com>
2025-01-02 10:27:33 +0800

    feat: 支持 serve 命令启动 msa-engine 调试

---
+++ mid/mock.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+function Router(req, res, next) {
+  const router = RouterMap[req.path];
+  if (router) {
+    router(req, res, next);
+  }
+  next();
+}
+
+
+const RouterMap = {
+  '/api/msa': (req, res) => {
+    const filePath = process.env.MSA_ENTRY_FILE;
+    const content = fs.existsSync(filePath) ? fs.readFileSync(filePath, 'utf8') : '';
+    res.json({
+        code: 200,
+        data: {
+          content
+        }
+    })
+  }
+}
+
+module.exports = Router;
---
+++ pub/index.js
@@ -0,0 +1,83 @@
+import Engine from "msa-engine";
+
+window.onload = () => {
+  init();
+}
+
+function init() {
+  const $jsonViewer = document.getElementById('json-viewer');
+
+  const $panelTitle = document.querySelectorAll('.panel-title');
+
+  const $generateBtn = document.getElementById('generate-btn');
+
+  const $errorDialog = document.getElementById('error-dialog');
+  const $errorDialogContent = document.getElementById('error-dialog-content');
+  const $errorDialogClose = document.getElementById('error-dialog-close');
+
+  const yamlViewer = window.CodeMirror.fromTextArea(document.getElementById('yaml-viewer'), {
+    mode: 'yaml',
+    readOnly: true,
+    theme: 'dracula',
+    lineNumbers: true,
+  });
+
+  const yamlEditor = window.CodeMirror.fromTextArea(document.getElementById('yaml-viewer-editor'), {
+    mode: 'yaml',
+    theme: 'dracula',
+    lineNumbers: true,
+  });
+
+  function generate(text) {
+    const engine = new Engine();
+
+    engine.parse(text).then((parseEngine) => {
+
+      yamlViewer.setValue(parseEngine.create());
+
+      const operationJson = parseEngine.getOperations();
+      $jsonViewer.textContent = JSON.stringify(operationJson, null, 2);
+    }).catch(err => {
+      $errorDialogContent.textContent = err.message;
+      $errorDialog.showModal();
+      console.error(err);
+    });
+  }
+
+  $panelTitle.forEach(($title) => {
+    $title.addEventListener('click', () => {
+      $title.classList.toggle('active');
+    })
+  });
+
+  $errorDialogClose.addEventListener('click', () => {
+    $errorDialog.close();
+  });
+
+
+
+  yamlViewer.setSize('100%', '100%');
+
+  yamlEditor.setSize('100%', '100%');
+
+  $generateBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const content = yamlEditor.getValue();
+    generate(content);
+  })
+
+
+
+
+  fetch("/api/msa?filePath=./msa/Msa.yml").then(res => res.json()).then(({ data }) => {
+    if (data.content) {
+      yamlEditor.setValue(data.content);
+      generate(data.content);
+    }
+  });
+}
+
+
+
+
